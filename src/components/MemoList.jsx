94904f40a00393bf7eaf3bffcf614b1e17c2d359
be-chan <70454487+be-chan@users.noreply.github.com>
2022-08-07 18:40:17 +0900

    Connect screens with react-navigation

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,39 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View, Text, StyleSheet, TouchableOpacity, Alert,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
 
 export default function MemoList() {
+  const navigation = useNavigation();
   return (
     <View>
-      <View style={styles.memoListItem}>
+      <TouchableOpacity
+        style={styles.memoListItem}
+        onPress={() => { navigation.navigate('MemoDetail'); }}
+      >
         <View>
           <Text style={styles.memoListItemTitle}>買い物リスト</Text>
           <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
         </View>
-        <TouchableOpacity>
+        <TouchableOpacity
+          style={styles.memoDelete}
+          onPress={() => { Alert.alert('Are you sure?'); }}
+        >
           <AntDesign name="close" size={16} color="#B0B0B0" />
         </TouchableOpacity>
-      </View>
-
-      <View style={styles.memoListItem}>
-        <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-        </View>
-        <TouchableOpacity>
-          <AntDesign name="close" size={16} color="#B0B0B0" />
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.memoListItem}>
-        <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-        </View>
-        <TouchableOpacity>
-          <AntDesign name="close" size={16} color="#B0B0B0" />
-        </TouchableOpacity>
-      </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -57,5 +47,8 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 16,
     color: '#848484',
+  },
+  memoDelete: {
+    padding: 8,
   },
 });
--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,12 +1,13 @@
+import { func } from 'prop-types';
+import { shape } from 'prop-types';
 import React from 'react';
 import { View, ScrollView, Text, StyleSheet } from 'react-native';
-import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
 
-export default function MemoDetailScreen() {
+export default function MemoDetailScreen(props) {
+  const { navigation } = props;
   return (
     <View style={styles.container}>
-      <AppBar />
       <View style={styles.memoHeader}>
         <Text style={styles.memoTitle}>買い物リスト</Text>
         <Text style={styles.memoDate}>2020年12月24日 10:00</Text>
@@ -18,10 +19,20 @@ export default function MemoDetailScreen() {
           本文用なので使いたかを間違えると不自然に見えることもありますので要注意。
         </Text>
       </ScrollView>
-      <CircleButton name="edit" style={{ top: 160, bottom: 'auto' }} />
+      <CircleButton
+        name="edit"
+        style={{ top: 60, bottom: 'auto' }}
+        onPress={() => { navigation.navigate('MemoEdit'); }}
+      />
     </View>
   );
 }
+
+MemoDetailScreen.propTypes = {
+  navigation: shape({
+    navigate: func,
+  })
+};
 
 const styles = StyleSheet.create({
   container: {
