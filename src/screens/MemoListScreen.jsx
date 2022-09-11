d8c5b35f99b253d73ae1bb707cb94b65962c5c57
be-chan <70454487+be-chan@users.noreply.github.com>
2022-09-11 19:45:36 +0900

    MemoList FlatList

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,19 +1,47 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Alert } from 'react-native';
+import firebase from 'firebase';
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  const [memos, setMemos] = useState([]);
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => <LogOutButton />
     });
   }, []);
+
+  useEffect(() => {
+    const db = firebase.firestore();
+    const { currentUser } = firebase.auth();
+    let unsubscribe = () => {};
+    if (currentUser) {
+      const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
+      unsubscribe = ref.onSnapshot((snapshot) => {
+        const userMemos = [];
+        snapshot.forEach((doc) => {
+          console.log(doc.id, doc.data());
+          const data = doc.data();
+          userMemos.push({
+            id: doc.id,
+            bodyText: data.bodyText,
+            updatedAt: data.updatedAt.toDate(),
+          });
+        }, (error) => {
+          console.log(error);
+          Alert.alert('データの読み込みに失敗しました。')
+        });
+        setMemos(userMemos);
+      });
+    }
+    return unsubscribe;
+  }, []);
   return (
     <View style={styles.container}>
-      <MemoList />
+      <MemoList memos={memos} />
       <CircleButton
         name="plus"
         onPress={() => { navigation.navigate('MemoCreate'); }}
